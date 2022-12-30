d245560e7035c871c2dea779f6ad6d454b7da118
Abhishek Chaubey <abhishekchaubey430@gmail.com>
2022-12-30 17:36:00 +0530

    added databse and express validator

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,29 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// Create a user without Auth using Post /api/auth 
-router.post('/', (req, res) => {
-      console.log(req.body);
-      const user =  User(req.body);
-      user.save();
 
-      res.send(req.body);
+const { body, validationResult } = require('express-validator');
+
+
+// Create a user without Auth using Post /api/auth 
+router.post('/',[
+      body('name','Ennter a valid name').isLength({ min: 3 }),
+      body('email').isEmail(),
+      body('password').isLength({ min: 6 }),
+], (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          }).then(user => res.json(user)).catch(err => {console.log(err)
+            res.json({error: "Email already taken..", message:err.message})
+      })
+
+      
 })
       
 
