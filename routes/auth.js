c046e66ade2ab9e4005061c37e835032ba6dbc23
Abhishek Chaubey <abhishekchaubey430@gmail.com>
2022-12-31 12:31:24 +0530

    Updating validation of email amd creating thunderclient Collections to mange requests

--- routes/auth.js
+++ routes/auth.js
@@ -6,25 +6,42 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
 
-// Create a user without Auth using Post /api/auth 
-router.post('/',[
+// Create a user without Auth using Post /api/auth/createUser 
+router.post('/createUser',[
       body('name','Ennter a valid name').isLength({ min: 3 }),
       body('email').isEmail(),
       body('password').isLength({ min: 6 }),
-], (req, res) => {
+], async (req, res) => {
+      // if there are errors return bad request and the errors 
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-      User.create({
+try {
+      
+
+      // check weather the user email exists already
+      let user = await User.findOne({email:req.body.email});
+      if(user){
+            return res.status(400).json({error:"A user with this email already exists !"});
+      }
+       user = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
-          }).then(user => res.json(user)).catch(err => {console.log(err)
-            res.json({error: "Email already taken..", message:err.message})
-      })
+          })
+          
+            // .then(user => res.json(user))
+            // .catch(err => {console.log(err)
+                  // res.json({error: "Email already taken..", message:err.message
+            // })
+      // })
 
-      
+      res.json(user);
+} catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some error occured !");
+}
 })
       
 
